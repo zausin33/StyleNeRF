2a42f481bd0714d0ff5482c61a11011c48ad5e30
Jonas Zausinger <jonas.zausinger@iamds.com>
2023-02-10 16:16:53 +0100

    first commit

---
+++ nerf/RAIN.py
@@ -0,0 +1,159 @@
+import torch.nn as nn
+import torch
+from torch.nn import init
+
+vgg = nn.Sequential(
+    nn.Conv2d(3, 3, (1, 1)),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(3, 64, (3, 3)),
+    nn.ReLU(inplace=True),  # relu1-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 64, (3, 3)),
+    nn.ReLU(inplace=True),  # relu1-2
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 128, (3, 3)),
+    nn.ReLU(inplace=True),  # relu2-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 128, (3, 3)),
+    nn.ReLU(inplace=True),  # relu2-2
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 256, (3, 3)),
+    nn.ReLU(inplace=True),  # relu3-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(inplace=True),  # relu3-2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(inplace=True),  # relu3-3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(inplace=True),  # relu3-4
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 512, (3, 3)),
+    nn.ReLU(inplace=True),  # relu4-1, this is the last layer used
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(inplace=True),  # relu4-2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(inplace=True),  # relu4-3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(inplace=True),  # relu4-4
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(inplace=True),  # relu5-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(inplace=True),  # relu5-2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(inplace=True),  # relu5-3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(inplace=True)  # relu5-4
+)
+
+fc_encoder = nn.Sequential(
+        nn.Linear(1024, 1024),
+        nn.ReLU(inplace=True),
+        nn.Linear(1024, 1024),
+        nn.ReLU(inplace=True),
+        nn.Linear(1024, 1024)
+)
+
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    # print(classname)
+    if classname.find('Conv') != -1:
+        init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
+    elif classname.find('Linear') != -1:
+        init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
+    elif classname.find('BatchNorm2d') != -1:
+        init.uniform(m.weight.data, 1.0, 0.02)
+        init.constant(m.bias.data, 0.0)
+
+
+class Net(nn.Module):
+    def __init__(self, args, device):
+        super(Net, self).__init__()
+        encoder = vgg
+        encoder.load_state_dict(torch.load(args.vgg_pretrained_path, map_location=device))
+        encoder = nn.Sequential(*list(encoder.children())[:31])
+
+        self.fc_encoder = fc_encoder
+
+        self.fc_encoder.load_state_dict(torch.load(args.fc_encoder_pretrained_path, map_location=device))
+        encoder.to(device)
+        self.fc_encoder.to(device)
+        enc_layers = list(encoder.children())
+        self.enc_1 = nn.Sequential(*enc_layers[:4])  # input -> relu1_1
+        self.enc_2 = nn.Sequential(*enc_layers[4:11])  # relu1_1 -> relu2_1
+        self.enc_3 = nn.Sequential(*enc_layers[11:18])  # relu2_1 -> relu3_1
+        self.enc_4 = nn.Sequential(*enc_layers[18:31])  # relu3_1 -> relu4_1
+
+        
+        # fix the encoder
+        for name in ['enc_1', 'enc_2', 'enc_3', 'enc_4']:
+            for param in getattr(self, name).parameters():
+                param.requires_grad = False
+                
+        self.fc_encoder.apply(weights_init_kaiming)
+
+        # Fixed RAIN_net
+        for param in self.parameters():
+            param.requires_grad = False
+
+    # extract relu1_1, relu2_1, relu3_1, relu4_1 from input image
+    def encode_with_intermediate(self, input):
+        results = [input]
+        for i in range(4):
+            func = getattr(self, 'enc_{:d}'.format(i + 1))
+            results.append(func(results[-1]))
+        return results[1:]
+
+    
+    # extract relu4_1 from input image
+    def get_content_feat(self, input):
+        for i in range(4):
+            input = getattr(self, 'enc_{:d}'.format(i + 1))(input)
+        return input
+
+
+    def get_style_feat(self, input):
+        style_feats = self.encode_with_intermediate(input)
+        out_mean = []
+        out_std = []
+        out_mean_std = []
+        for style_feat in style_feats:
+            style_feat_mean, style_feat_std, style_feat_mean_std = self.calc_feat_mean_std(style_feat)
+            out_mean.append(style_feat_mean)
+            out_std.append(style_feat_std)
+            out_mean_std.append(style_feat_mean_std)
+        return style_feats, torch.cat(out_mean_std, dim=-1)
+
+    
+    def calc_feat_mean_std(self, input, eps=1e-5):
+        # eps is a small value added to the variance to avoid divide-by-zero.
+        size = input.size()
+        assert (len(size) == 4)
+        N, C = size[:2]
+        feat_var = input.view(N, C, -1).var(dim=2) + eps
+        feat_std = feat_var.sqrt().view(N, C)
+        feat_mean = input.view(N, C, -1).mean(dim=2).view(N, C)
+        return feat_mean, feat_std, torch.cat([feat_mean, feat_std], dim = 1)
+    
+
+    def get_hyper_input(self, style):
+        style_feats = self.encode_with_intermediate(style)
+        _, _, style_feat_mean_std = self.calc_feat_mean_std(style_feats[-1])
+
+        intermediate = self.fc_encoder(style_feat_mean_std)
+        intermediate_mean = intermediate[:, :512]
+
+        return intermediate_mean
---
+++ readme.md
@@ -0,0 +1,89 @@
+# Fast 3D Style Transfer using Neural Radiance Fields
+Applying Artistic Style Transfer to Neural Radiance Field (NeRF), a neural network for
+rendering 3D scenes.
+
+This code is based on torch-ngp
+
+<img src="assets/fox.gif" height="342"/>
+<img src="assets/fox_styled0.gif" height="342"/>
+<img src="assets/fox_styled1.gif" height="342"/>
+
+# Install
+
+### Install with pip
+```bash
+pip install -r requirements.txt
+
+# (optional) install the tcnn backbone
+pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
+```
+
+### Install with conda
+```bash
+conda env create -f environment.yml
+conda activate torch-ngp
+```
+
+### Build extension (optional)
+By default, we use [`load`](https://pytorch.org/docs/stable/cpp_extension.html#torch.utils.cpp_extension.load) to build the extension at runtime.
+However, this may be inconvenient sometimes.
+Therefore, we also provide the `setup.py` to build each extension:
+```bash
+# install all extension modules
+bash scripts/install_ext.sh
+
+# if you want to install manually, here is an example:
+cd raymarching
+python setup.py build_ext --inplace # build ext only, do not install (only can be used in the parent directory)
+pip install . # install to python path (you still need the raymarching/ folder, since this only install the built extension.)
+```
+
+### Tested environments
+* Ubuntu 20 with torch 1.10 & CUDA 11.3 on a TITAN RTX.
+* Ubuntu 16 with torch 1.8 & CUDA 10.1 on a V100.
+* Windows 10 with torch 1.11 & CUDA 11.3 on a RTX 3070.
+
+Currently, `--ff` only supports GPUs with CUDA architecture `>= 70`.
+For GPUs with lower architecture, `--tcnn` can still be used, but the speed will be slower compared to more recent GPUs.
+
+
+# Usage
+
+The same data format as instant-ngp is used e.g., [fox](https://github.com/NVlabs/instant-ngp/tree/master/data/nerf/fox). 
+We already provided some datasets under `./data`.
+
+
+First time running will take some time to compile the CUDA extensions.
+
+For training the first stage:
+```bash
+python main_nerf.py data/fox --workspace trial_nerf -O --stage=first
+```
+
+For training the second stage:
+```bash
+python main_nerf.py data/fox -O --workspace trial_nerf --stage=second --style_img_path=./style_img/0.jpg --patch_size=128 --num_rays=16384 --gui --learn_layer color encoder sigma --style_loss=gram --subsampling=downsample --content_weight=3e-5
+```
+
+For generating images (images are generated in trial_nerf/results):
+```bash
+python main_nerf.py data/fox --workspace trial_nerf -O --ckpt="<MODEL_PATH>" --test
+```
+
+# Acknowledgement
+torch-ngp
+```
+@misc{torch-ngp,
+    Author = {Jiaxiang Tang},
+    Year = {2022},
+    Note = {https://github.com/ashawkey/torch-ngp},
+    Title = {Torch-ngp: a PyTorch implementation of instant-ngp}
+}
+
+@article{tang2022compressible,
+    title = {Compressible-composable NeRF via Rank-residual Decomposition},
+    author = {Tang, Jiaxiang and Chen, Xiaokang and Wang, Jingbo and Zeng, Gang},
+    journal = {arXiv preprint arXiv:2205.14870},
+    year = {2022}
+}
+```
